5a8edd6c09ea08b07c257fdfbfb37f65aae003bc
Alan Brian <36174194+alanbriangh@users.noreply.github.com>
2019-11-19 00:42:47 -0300

    Add files via upload

--- buscarSubdominios.sh
+++ buscarSubdominios.sh
@@ -15,3 +15,17 @@ cat $1/domains.txt | ~/go/bin/httprobe/./main | tee -a $1/alive.txt
 #formatting the data to json
 cat $1/alive.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $1/alive.json
 cat $1/domains.txt | python -c "import sys; import json; print (json.dumps({'domains':list(sys.stdin)}))" > $1/domains.json
+#change directory
+cd $1
+#create new directories
+mkdir headers
+mkdir responsebody
+#sending cURL requests to fetch headers and response body 
+CURRENT_PATH=$(pwd)
+
+for x in $(cat alive.txt)
+do
+        NAME=$(echo $x | awk -F/ '{print $3}')
+        curl -X GET -H "X-Forwarded-For: evil.com" $x -I > "$CURRENT_PATH/headers/$NAME"
+        curl -s -X GET -H "X-Forwarded-For: evil.com" -L $x > "$CURRENT_PATH/responsebody/$NAME"
+done
