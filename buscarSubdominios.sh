5ebb8d66bec96f805f3e760e870f54d625fa02d8
Alan Brian <36174194+alanbriangh@users.noreply.github.com>
2019-11-19 01:05:09 -0300

    Add files via upload

--- buscarSubdominios.sh
+++ buscarSubdominios.sh
@@ -29,3 +29,40 @@ do
         curl -X GET -H "X-Forwarded-For: evil.com" $x -I > "$CURRENT_PATH/headers/$NAME"
         curl -s -X GET -H "X-Forwarded-For: evil.com" -L $x > "$CURRENT_PATH/responsebody/$NAME"
 done
+
+mkdir scripts
+mkdir scriptsresponse
+RED='\033[0;31m'
+NC='\033[0m'
+CUR_PATH=$(pwd)
+for x in $(ls "$CUR_PATH/responsebody")
+do
+        printf "\n\n${RED}$x${NC}\n\n"
+        END_POINTS=$(cat "$CUR_PATH/responsebody/$x" | grep -Eoi "src=\"[^>]+></script>" | cut -d '"' -f 2)
+        for end_point in $END_POINTS
+        do
+                len=$(echo $end_point | grep "http" | wc -c)
+                mkdir "scriptsresponse/$x/"
+                URL=$end_point
+                if [ $len == 0 ]
+                then
+                        URL="https://$x$end_point"
+                fi
+                file=$(basename $end_point)
+                curl -X GET $URL -L > "scriptsresponse/$x/$file"
+                echo $URL >> "scripts/$x"
+        done
+done
+
+#looping through the scriptsresponse directory
+mkdir endpoints
+CUR_DIR=$(pwd)
+for domain in $(ls scriptsresponse)
+do
+        #looping through files in each domain
+        mkdir endpoints/$domain
+        for file in $(ls scriptsresponse/$domain)
+        do
+                ruby ~/Tools/relative-url-extractor/extract.rb scriptsresponse/$domain/$file >> endpoints/$domain/$file 
+        done
+done
